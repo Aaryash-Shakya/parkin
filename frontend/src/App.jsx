a7f96a9fbe2e144cdb10bde07f0823a266747a02
andro-jr <prabin20panta@gmail.com>
2025-01-09 19:55:14 +0545

    feat: user auth api setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,12 @@ import MyRoutes from "./MyRoutes";
 import { ToastContainer } from "react-toastify";
 import { useLoadingStore } from "./store/loading.store";
 import LoadingIndicator from "./components/LoadingIndicator";
+import { createUser } from "./api/auth";
 
 function App() {
   const { loading } = useLoadingStore();
+
+  // createUser();
 
   return (
     <>
