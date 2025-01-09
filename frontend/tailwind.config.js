c7dd6c96a4fa0f70ba778603d249f98c13c51718
2002lyon <leon.lama2002@gmail.com>
2025-01-09 16:56:18 +0545

    tailwind color pallatte configuration added.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,40 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "1.5rem",
+        lg: "2rem",
+      },
+      screens: {
+        sm: "920px",
+        md: "1210px",
+        lg: "1410px",
+        xl: "1610px",
+      },
+    },
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "rgb(126, 1, 255)",
+          light: "",
+        },
+        secondary: {
+          DEFAULT: "rgb(254, 239, 73)",
+        },
+        background: {
+          DEFAULT: "rgb(224, 224, 225)",
+        },
+        black: {
+          DEFAULT: "rgb(21, 21, 20)",
+          500: "#262626",
+        },
+        white: {
+          DEFAULT: "rgb(249, 249, 248)",
+        },
+      },
+    },
   },
   plugins: [],
 };
