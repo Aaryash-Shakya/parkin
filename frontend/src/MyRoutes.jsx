cf61a0bcadd6d5401f3bceb455e7c1b0b7e0f21e
andro-jr <prabin20panta@gmail.com>
2025-01-09 17:47:36 +0545

    setup: route setup and toastify setup

---
+++ frontend/src/MyRoutes.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+const MyRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default MyRoutes;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import { toast } from "react-toastify";
+
+const Home = () => {
+  toast.info("testing toastify");
+
+  return <div>Home</div>;
+};
+
+export default Home;
