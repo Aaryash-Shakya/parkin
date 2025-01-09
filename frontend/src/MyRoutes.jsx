ad0b25ca130c9042ac328a8e86edb80ebf4fec62
andro-jr <prabin20panta@gmail.com>
2025-01-09 18:40:45 +0545

    add: auth sign in page

--- frontend/src/MyRoutes.jsx
+++ frontend/src/MyRoutes.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import SignIn from "./pages/SignIn";
 
 const MyRoutes = () => {
   return (
     <Router>
       <Routes>
         <Route index element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/about" element={<About />} />
       </Routes>
     </Router>
