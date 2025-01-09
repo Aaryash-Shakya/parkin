057298ddeb7fc34c7a133056665fa0de3c37a627
andro-jr <prabin20panta@gmail.com>
2025-01-09 19:27:57 +0545

    feat: loading indicator configured

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import MyRoutes from "./MyRoutes";
 import { ToastContainer } from "react-toastify";
+import { useLoadingStore } from "./store/loading.store";
+import LoadingIndicator from "./components/LoadingIndicator";
 
 function App() {
+  const { loading } = useLoadingStore();
+
   return (
     <>
+      {loading && <LoadingIndicator />}
       <ToastContainer />
       <MyRoutes />
     </>
