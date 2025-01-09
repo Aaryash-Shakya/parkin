c4d82f648c3510441bcd62de36f49c39b405de25
andro-jr <prabin20panta@gmail.com>
2025-01-09 19:14:59 +0545

    add: signup page setup

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,68 +1,68 @@
-import React, { useContext, useEffect, useState } from "react";
 import FormInput from "../components/form/FormInput";
 import Button from "../components/form/Button";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 import Container from "../components/Container";
-import { toast } from "react-toastify";
 
-const SignIn = () => {
+const SignUp = () => {
   const [inputData, setInputData] = useState({
+    username: "",
     email: "",
     password: "",
   });
 
-  const validateUserInput = (userInput) => {
-    const { email, password } = userInput;
-    const isValidEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
-    if (!email.trim()) return { ok: false, err: "Email is missing" };
-    if (!isValidEmail.test(email))
-      return { ok: false, err: "Email is invalid" };
-
-    return { ok: true };
-  };
+  // console.log(inputData);
 
   const handleChange = (e) => {
     e.preventDefault();
+
     const { value, name } = e.target;
     setInputData({ ...inputData, [name]: value });
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const { ok, err } = validateUserInput(inputData);
-
-    if (err) return toast.error(err);
-
     const payload = {
+      name: inputData.username.toLowerCase(),
       email: inputData.email,
       password: inputData.password,
     };
 
-    console.log("user is logging in with", payload);
+    console.log("signing up the user", payload);
   };
 
   return (
     <Container>
-      <div className="container max-w-[1200px] m-auto login min-h-[70vh]">
+      <div className="container max-w-[1300px] m-auto login min-h-[70vh]">
         <div>
-          <div className="hidden lg:flex lg:p-0 xl:p-10 side__border items-center flex-col">
-            <img src="/login-theme.jpg" alt="parkin " />
+          <div className="hidden lg:flex lg:p-0 xl:p-10 side__border items-center flex-col aspect-square">
+            <img
+              src="/signup-theme.jpg"
+              alt="parkin"
+              className="w-full h-full object-cover object-bottom"
+            />
           </div>
         </div>
 
-        <div className="flex flex-col items-center my-auto pl:0 xl:pl-20">
-          <h3 className="text-4xl text-primary font-bold mt-0 md:mt-10">
-            Log in to ParkIn
+        <div className="flex flex-col items-center my-auto p-0 lg:p-5 2xl:p-20 pr-0  overflow-y-auto overflow-x-hidden">
+          <h3 className="text-4xl text-primary font-bold mt-0 md:mt-0">
+            Sign Up
           </h3>
-          <p className="mb-8 mt-4 max-w-[350px] text-center">
-            Welcome to ParkIn.
+          <p className="my-8 mt-4 max-w-[350px] lg:max-w-[400px] text-center">
+            Smarter Parking, Anytime, Anywhere!
           </p>
           <form onSubmit={handleSubmit}>
             <FormInput
-              placeholder="abc@example.com"
+              placeholder="johnDoe213"
               type="text"
+              name="username"
+              value={inputData.username}
+              onChange={handleChange}
+            />
+            <FormInput
+              placeholder="abc@example.com"
+              type="email"
               name="email"
               value={inputData.email}
               onChange={handleChange}
@@ -76,17 +76,17 @@ const SignIn = () => {
             />
 
             <Button
-              title="Login"
+              title="Sign Up"
               styles="bg-primary text-white hover:bg-blue-700 transition-all duration-0 hover:duration-150 ease-in-out"
             />
 
             <span className="text-sm text-gray-400 flex gap-2 mt-8 text-center items-center justify-center">
-              <p>New to ParkIn?</p>
+              <p>Already have an account?</p>
               <Link
-                to="/sign-up"
+                to="/sign-in"
                 className=" hover:text-red-600 transition-all duration-0 hover:duration-50 ease-in-out hover:underline font-semibold text-primary"
               >
-                Create Account
+                Sign In
               </Link>
             </span>
           </form>
@@ -96,4 +96,4 @@ const SignIn = () => {
   );
 };
 
-export default SignIn;
+export default SignUp;
