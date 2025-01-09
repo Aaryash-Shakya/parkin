a02ab6502c56bdc4bec46d63b205978f3bf678cc
KaushalKishorMishra <077bcsit016.kaushal@scst.edu.np>
2025-01-09 19:18:06 +0545

    init: open-street-map

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import BottomNavbar from "../components/BottomNavbar";
+import Map from "../components/Map";
 import Searchbar from "../components/Searchbar";
 
 const Home = () => {
+  const [selectPosition, setSelectPosition] = useState(null);
+
   return (
     <div className="bg-background h-screen relative">
-      <Searchbar />
+      <Searchbar
+        selectPosition={selectPosition}
+        setSelectPosition={setSelectPosition}
+      />
+      {/* Map */}
+      <Map
+        selectPosition={{
+          lat: selectPosition?.lat || 51.505,
+          lon: selectPosition?.lon || -0.09,
+        }}
+      />
       <BottomNavbar />
     </div>
   );
