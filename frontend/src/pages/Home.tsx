0b5b69f3f85b70f688f1ad1dd1acaa70c8eca0a6
andro-jr <prabin20panta@gmail.com>
2025-01-09 16:35:01 +0545

    add: auth context and api setup

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { ToastContainer } from "react-toastify";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <ToastContainer
+        position="top-right"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+      Home
+    </div>
+  );
 };
 
 export default Home;
