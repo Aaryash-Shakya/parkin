cb8afef6e411b20daadab8c3d2fe745d71914d2e
andro-jr <prabin20panta@gmail.com>
2025-01-10 08:31:20 +0545

    add: additional inputs for reservation

--- frontend/src/components/reservations/ReserveParking.jsx
+++ frontend/src/components/reservations/ReserveParking.jsx
@@ -4,21 +4,42 @@ import Button from "../form/Button";
 import FormInput from "../form/FormInput";
 import CustomDateTimePicker from "../form/CustomDateTimePicker";
 import { toast } from "react-toastify";
+import FormSelect from "../form/FormSelect";
 
 const ReserveParking = () => {
   const { reservation } = useReservationStore();
+
+  const vehicleTypeOptions = [
+    {
+      id: "2Wheeler",
+      displayName: "2 Wheeler",
+      space: 1,
+    },
+    {
+      id: "4Wheeler",
+      displayName: "4 Wheeler",
+      space: 3,
+    },
+  ];
+
+  // States for inputs
   const [fromDateTime, setFromDateTime] = useState(new Date());
   const [toDateTime, setToDateTime] = useState(
     new Date(new Date().getTime() + 7200000) // adding 2 hours as milliseconds
   );
+  const [numberPlate, setNumberPlate] = useState("");
+  const [vehicleType, setVehicleType] = useState(vehicleTypeOptions[0].id);
   const [error, setError] = useState("");
 
   const validateDates = () => {
     const differenceInMinutes =
       (toDateTime.getTime() - fromDateTime.getTime()) / 60000;
     if (differenceInMinutes < 30) {
+      setError(
+        "Reservation must be made in future with at least 30 minutes duration!"
+      );
       toast.error(
-        "Reservation must me made in future with at-least 30min duration!"
+        "Reservation must be made in future with at least 30 minutes duration!"
       );
       return false;
     }
@@ -30,7 +51,12 @@ const ReserveParking = () => {
     e.preventDefault();
     if (validateDates()) {
       // Proceed with form submission
-      console.log("Reservation confirmed!");
+      console.log({
+        fromDateTime,
+        toDateTime,
+        numberPlate,
+        vehicleType,
+      });
     }
   };
 
@@ -46,12 +72,12 @@ const ReserveParking = () => {
         <img src="/reservation-preview.png" alt="" />
         <form onSubmit={handleSubmit}>
           <FormInput
-            placeholder="abc@example.com"
+            placeholder="Display Name"
             type="text"
             name="display_name"
             label="Location"
             value={reservation.display_name}
-            onChange={() => {}}
+            onChange={() => {}} // disable display name change
           />
 
           <CustomDateTimePicker
@@ -70,6 +96,24 @@ const ReserveParking = () => {
             }}
             value={toDateTime}
             label="To"
+          />
+
+          <FormInput
+            placeholder="3467"
+            type="number"
+            name="numberPlate"
+            label="Number Plate"
+            value={numberPlate}
+            maxLength={4}
+            onChange={(e) => setNumberPlate(e.target.value)}
+          />
+
+          <FormSelect
+            name="vehicleType"
+            label="Vehicle Type"
+            value={vehicleType}
+            onChange={(e) => setVehicleType(e.target.value)}
+            options={vehicleTypeOptions}
           />
 
           {error && <p className="text-red-500 mt-2">{error}</p>}
